7ef718da91c2d164004cdd9741ba5469f3535bd2
yoan <yoan@jig.jp>
2016-06-03 14:48:45 +0900

    MyApplicationから不要FeatureやFilterを削除

--- src/main/java/sample/MyApplication.java
+++ src/main/java/sample/MyApplication.java
@@ -15,12 +15,27 @@ import java.util.List;
  * Created by yoan on 2016/06/02.
  */
 public class MyApplication extends ResourceConfig {
+    private final String ROOT_PACKAGE = this.getClass().getPackage().getName();
+    private final String CONTROLLERS = ROOT_PACKAGE + ".controllers";
 
     @Inject
     public MyApplication(ServiceLocator serviceLocator) {
-        System.out.println("MyApplication constructor");
-        packages(this.getClass().getPackage().getName() + ".controllers");
-        GuiceListener.init(new HK2IntoGuiceBridge(serviceLocator));
+        this(serviceLocator, getModule(serviceLocator));
     }
 
+    public MyApplication(ServiceLocator serviceLocator, Module... modules) {
+        packages(CONTROLLERS);
+
+        register(HttpMethodOverrideFilter.class);
+
+        GuiceListener.init(serviceLocator, modules);
+    }
+
+    private static Module getModule(ServiceLocator serviceLocator) {
+        List<Module> result = new ArrayList<>();
+        if (serviceLocator != null) {
+            result.add(new HK2IntoGuiceBridge(serviceLocator));
+        }
+        return Modules.combine(result);
+    }
 }
